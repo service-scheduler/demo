a852c2bde3a2af4fa770857d51a47a70325e68c7
Josh Hamit <josh.hamit@gmail.com>
2014-10-20 17:34:43 -0500

    Added click functionality

--- installation.js
+++ installation.js
@@ -1,7 +1,12 @@
+var clickedRedirect = null;
 //add button
-var button = '<button class="shopNow" style="float:right; margin-right: -50px; border:0">Schedule Installation</button>"';
+var button = '<button class="shopNow serviceInstallation" style="float:right; margin-right: -50px; border:0">Schedule Installation</button>"';
 $('.shopNow').parent().after(button);
 
 //attach click handler
+$('.serviceInstallation').on('click', function(event) {
+    clickedId = $(this).prev().attr('href');
+    console.log(clickedRedirect);
+});
 //load date picker
 //redirect to checkout
