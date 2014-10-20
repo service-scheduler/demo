92e6c948df5eeeddacf4e96cb86f7f4ca6f30aa7
Josh Hamit <josh.hamit@gmail.com>
2014-10-20 17:08:11 -0500

    initial add of installation button

--- installation.js
+++ installation.js
@@ -1,1 +1,7 @@
-alert('hi');
+//add button
+var button = '<button class="shopNow" style="float:right; margin-right: -50px; border:0">Schedule Installation</button>"';
+$('.shopNow').parent().after(button);
+
+//attach click handler
+//load date picker
+//redirect to checkout
