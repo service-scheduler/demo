0177f0c673cc7b91d1186af29853994cf97387ac
Josh Hamit <josh.hamit@gmail.com>
2014-10-20 21:01:31 -0500

    Got date picker working and redirecting to cart with products

--- installation.js
+++ installation.js
@@ -11,22 +11,33 @@ $('.serviceInstallation').on('click', function(event) {
     WALMART.quicklook.LoadQuickView(clickedId);
 	productID = clickedId.substr(-8);
 	console.log("ProductID: " + productID);
-	//sendToCart(productID)
+    loadDatePickerInModal(clickedId);
 });
+
 //load date picker
+function loadDatePickerInModal(clickedId) {
+    $('.wm-widget-overlay-closeBox').ready(function(){
+        $('.ui-dialog').after('<input value="Set Date and Checkout" class="shopNow" style="position: absolute; top: 587px; right: 575px; z-index: 999999; background:linear-gradient(to bottom, #f6962a 50%, #f2671e 50%) no-repeat scroll 0 0 #f6962a; border-radius: 5px; color: white; size:15px; font-weight: bold; padding: 10px 24px; border:0;" type="button" id="datepicker" />')
+        $( "#datepicker" ).datepicker();
+        $('#datepicker').on('change', function(event) {
+            $(this).attr('value', "Set Date and Checkout");
+            sendToCart(productID);
+            return false;
+        });
+    });
+};
+
 //redirect to checkout
-
-
 function sendToCart(id) {
 	var urlA = "http://www.walmart.com/catalog/select_product.do?product_id=";
 	var urlB = "&seller_id=0&path=&bti=&item_page=new&originURL=originURL%28%29%3B&matureContentAccepted=false&isAccItem=false&pcpSellerId=0&store_id=-1&product_put_price=0&hasCarePlans=false&hasHomeInstallation=false&carePlanOverlaySwitch=true&homeInstallationSwitch=true&itemOCCType=A&checksum=-1&qty_C1I194735350=4&ship_to_store.x=47&ship_to_store.y=13&buyNow=false&qty=4&path=&bti=&S2S_zip=Enter+ZIP+code&qty_C1I194735350SOI=1&zip=Enter+ZIP+code";
 	window.location.href = urlA + "" + id + "" + urlB;
-}
+};
 
 function wait(waitsecs){
 setTimeout(donothing(), 'waitsecs');
-}
+};
 
 function donothing() {
 
-}
+};
